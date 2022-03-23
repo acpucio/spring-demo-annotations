0bac6471eaae496f7b6cf670f9c5aa032a34136e
acpucio <acpucio@gmail.com>
2022-03-23 00:30:43 -0500

    Dependency injection using constructor

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -10,11 +10,15 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from the spring container
-		Coach theCoach = context.getBean("kungFuCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		// call method to get the daily fortune
+		
+		System.out.println(theCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
--- src/com/luv2code/springdemo/KungFuCoach.java
+++ src/com/luv2code/springdemo/KungFuCoach.java
@@ -5,10 +5,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class KungFuCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
 	@Override
 	public String getDailyWorkout() {
 
 		return "Practice your Dragon Tornado Kicks";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune() ;
 	}
 
 }
