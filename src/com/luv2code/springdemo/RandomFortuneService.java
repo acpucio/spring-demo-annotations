1b1179a41e893e0def6d1938902bfb36525173b7
acpucio <acpucio@gmail.com>
2022-03-24 00:31:29 -0500

    More DI and @Qualifier practice

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,14 +1,31 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
-public class RandomService implements FortuneService {
+public class RandomFortuneService implements FortuneService {
 
+	// create an array of strings
+	private String[] data = {
+			"Beware of the wolf in sheep's coding",
+			"Diligence is the mother of good luck",
+			"The Journey is the reward"
+	};
+	
+	// create a randome number genearator
+	private Random myRandom = new Random();
+	
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// Pick a random string from the array
+		int index = myRandom.nextInt(data.length);
+
+		String theFortune = data[index];
+		
+		return theFortune;
 	}
 
 }
